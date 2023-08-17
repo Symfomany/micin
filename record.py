c40e98d9da34c22982399a63d8485f58e4ce102c
taiwa-projects <taiwa.agency@gmail.com>
2023-08-17 15:55:15 +0200

    ok

---
+++ record.py
@@ -0,0 +1,20 @@
+import sounddevice as sd
+import numpy as np
+import wavio
+
+# Paramètres d'enregistrement
+RATE = 16000  # Taux d'échantillonnage (16KHz)
+CHANNELS = 1  # Mono
+DTYPE = np.int16  # 16 bits
+SECONDS = 5  # Durée de l'enregistrement, ici 5 secondes. Vous pouvez le modifier.
+
+# Enregistrement
+print("Enregistrement...")
+audio = sd.rec(int(SECONDS * RATE), samplerate=RATE, channels=CHANNELS, dtype=DTYPE)
+
+# Attendez que l'enregistrement soit terminé
+sd.wait()
+
+# Sauvegarde au format WAV
+wavio.write("enregistrement.wav", audio, RATE, sampwidth=2)
+print("Enregistrement terminé et sauvegardé dans 'enregistrement.wav'")
